c4690593df2c7b198ac932d9bd8075db0d60b0e2
Francisco <franciscojosebellosalcedo@gmail.com>
2024-01-30 13:29:39 -0500

    loader is ready in page

--- src/components/layaut/Layaut.jsx
+++ src/components/layaut/Layaut.jsx
@@ -10,61 +10,70 @@ import { getAllProducts } from "../../service/product.service";
 import { setProducts } from "../../features/product/product";
 
 const Layaut = () => {
-	const token=useSelector((state)=>state.token.data.token);
-	const dispatch=useDispatch();
-	const [isLoader,setIsLoader]=useState(false);
+	const token = useSelector((state) => state.token.data.token);
+	const dispatch = useDispatch();
+	const [isLoader, setIsLoader] = useState(false);
 
-	const getProducts= async () => {
+	const getProducts = async () => {
+		setIsLoader(true);
 		try {
-			const responseProducts=(await getAllProducts(token)).data;
-			if(responseProducts.status===200 && responseProducts.response){
-				const data=responseProducts.data;
+			const responseProducts = (await getAllProducts(token)).data;
+			if (responseProducts.status === 200 && responseProducts.response) {
+				const data = responseProducts.data;
 				dispatch(setProducts(data));
 			}
 		} catch (error) {
 			console.log(error)
 		}
+		setIsLoader(false);
+
 	}
 	const getCollections = async () => {
+		setIsLoader(true);
+
 		try {
-			const responseCollections=(await getAllCollections(token)).data;
-			if(responseCollections.status===200 && responseCollections.response){
-				const data=responseCollections.data;
+			const responseCollections = (await getAllCollections(token)).data;
+			if (responseCollections.status === 200 && responseCollections.response) {
+				const data = responseCollections.data;
 				dispatch(setCollections(data));
 			}
 		} catch (error) {
 			console.log(error)
 		}
+		setIsLoader(false);
+
 	}
 
 	const getCategories = async () => {
+		setIsLoader(true);
+
 		try {
-			const responseCategory=(await getAllCategories(token)).data;
-			if(responseCategory.status===200 && responseCategory.response){
-				const data=responseCategory.data;
+			const responseCategory = (await getAllCategories(token)).data;
+			if (responseCategory.status === 200 && responseCategory.response) {
+				const data = responseCategory.data;
 				dispatch(setCategories(data));
 			}
 		} catch (error) {
 			console.log(error)
 		}
+		setIsLoader(false);
+
 	}
 
-	const getAllRegistersModules=async ()=>{
-		setIsLoader(true);
+	const getAllRegistersModules = async () => {
 		getCategories();
 		getCollections();
 		getProducts();
-		setIsLoader(false);
 	}
 
-	useEffect(()=>{
+	useEffect(() => {
 		getAllRegistersModules();
 	});
 
 	return (
 		<>
 			{
-				isLoader=== true ? <Loader/> :<RoutesApp />
+				isLoader === true ? <Loader /> : <RoutesApp />
 			}
 		</>
 	)
