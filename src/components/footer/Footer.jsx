d89be95ed29fe87eb73744145299b0c5551a4d80
Francisco <franciscojosebellosalcedo@gmail.com>
2024-01-31 20:15:00 -0500

    footer complete

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,17 +14,29 @@ const Footer = () => {
 				</div>
 
 				<div className="content_info_footer">
-					<h1 className="info_footer_title">Ele rose accesorios</h1>
+					<h1 className="info_footer_title">Ele rose</h1>
 					<ul className="list_info_ele_rose">
 						<li>Sobre nosotros</li>
 						<li>Contacto</li>
 					</ul>
 				</div>
-				<div className="content_info_footer">
 
+				<div className="content_info_footer">
+					<figure className="icon_media">
+						<i className="uil uil-instagram"></i>
+					</figure>
+					<figure className="icon_media">
+						<i className="uil uil-whatsapp"></i>
+					</figure>
+					<figure className="icon_media">
+						<img src={require("../../assest/tik-tok.png")} alt="" />
+					</figure>
 				</div>
 			</section>
-			<section></section>
+
+			<section className="info_footer_page">
+				&#169; 2024 Ele Rose Accesorios - Todos los derechos reservados | Desarrollado por Francisco José Bello Salcedo
+			</section>
 		</footer>
 	)
 }
