ae587bf9d3aa91572d4e68b6497eb5c7d9d6dc5c
Francisco <franciscojosebellosalcedo@gmail.com>
2024-02-12 11:21:41 -0500

    page cart ready

--- src/components/tableCart/TableCart.jsx
+++ src/components/tableCart/TableCart.jsx
@@ -1,62 +1,140 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./TableCart.css";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../../constants/constants";
+import { removeItemCart, setActiveCart, setActiveSendOrder, setCart } from "../../features/cart/cart";
+import { getTotalPriceCart, isValidObject, sendOrderUser } from "../../utils/utils";
+import ModalOrderInfo from "../modalOrderInfo/ModalOrderInfo";
+import { useState } from "react";
+import { setIsOpenModal } from "../../features/user/user";
+import LoaderButton from "../loaderButton/LoaderButton";
 
 const TableCart = () => {
+	const listItemCart = useSelector((state) => state.cart.data.list);
+	const isActiveCart = useSelector((state) => state.cart.data.active);
+	const [alertOrder, setAlertOrder] = useState({ message: "", type: 0, title: "" });
+	const [isActiveModalInfoOrder, setIsActiveModalInfoOrder] = useState(false);
+	const [isLoader, setIsLoader] = useState(false);
+	const user = useSelector((state) => state.user.data.user);
+	const nav = useNavigate();
+	const dispatch = useDispatch();
+
+	const handlerOpenModalInfoOrder = () => {
+		setIsActiveModalInfoOrder(!isActiveModalInfoOrder);
+	}
+
+	const navigate = () => {
+		nav(ROUTES.ACCESORIES);
+		if (isActiveCart) {
+			dispatch(setActiveCart());
+		}
+	}
+
+	const removeItem = (index) => {
+		dispatch(removeItemCart(index));
+	}
+
+	const sendOrder = async (e) => {
+		e.preventDefault();
+		setIsLoader(true);
+		try {
+			if (isValidObject(user) === false && listItemCart.length) {
+				dispatch(setActiveSendOrder(true));
+				dispatch(setIsOpenModal());
+			} else {
+				if (listItemCart.length > 0) {
+					const responseOrder = await sendOrderUser(listItemCart, user, user.token);
+					if (responseOrder.status === 201 && responseOrder.response) {
+						if (isActiveCart) {
+							dispatch(setActiveCart());
+						}
+						dispatch(setCart([]));
+						setAlertOrder({
+							message: "Hemos recibido tu pedido y nos pondremos en contacto contigo en breve para confirmar todos los detalles y coordinar la entrega.",
+							title: responseOrder.message,
+							type: 1
+						});
+					} else {
+						setAlertOrder({
+							message: "Se produjo un error al enviar el pedido, por favor intente nuevamente.",
+							title: responseOrder.message,
+							type: 0
+						});
+					}
+					handlerOpenModalInfoOrder();
+				}
+			}
+		} catch (error) {
+			setAlertOrder({
+				message: "Se produjo un error al enviar el pedido, por favor intente nuevamente.",
+				title: "Error",
+				type: 0
+			});
+		}
+		setIsLoader(false);
+	}
+
+
 	return (
 		<section className="container_table_cart">
-			<table class="resp">
-				<thead>
-					<tr>
-						<th scope="col">Titulo</th>
-						<th scope="col">Año</th>
-						<th scope="col">Formato</th>
-						<th scope="col">Autor</th>
-						<th scope="col">Tamaño</th>
-						<th scope="col">Precio</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Intocable</td>
-						<td>2012</td>
-						<td>DVD</td>
-						<td>Françoise Cluzet</td>
-						<td>120 minutos</td>
-						<td>12,86€</td>
-					</tr>
-					<tr>
-						<td>El gen egoista</td>
-						<td>2010</td>
-						<td>Kindle</td>
-						<td>Richard Dawkins</td>
-						<td>424 páginas</td>
-						<td>11,50€</td>
-					</tr>
-					<tr>
-						<td>Mis Tardes Con Margueritte </td>
-						<td>2011</td>
-						<td>DVD</td>
-						<td>Gerard Depardieu</td>
-						<td>82 minutos</td>
-						<td>6,24€</td>
-					</tr>
-					<tr>
-						<td>Nicostratos</td>
-						<td>2011</td>
-						<td>Kindle</td>
-						<td>Eric Boisset</td>
-						<td>208 páginas</td>
-						<td>4,94€</td>
-					</tr>
-					<tr>
-						<td>Juegos en que participamos </td>
-						<td>2007</td>
-						<td>Libro</td>
-						<td>Eric Berne</td>
-						<td>232 páginas</td>
-						<td>1.076,28€</td>
-					</tr>
-				</tbody>
-			</table>
+			{
+				listItemCart && listItemCart.length > 0 ?
+					<>
+						<table className="resp">
+							<thead>
+								<tr>
+									<th scope="col">Producto</th>
+									<th scope="col">Precio</th>
+									<th scope="col">Disponibles</th>
+									<th scope="col">Cantidad</th>
+									<th scope="col">Total</th>
+									<th scope="col">Acciones</th>
+								</tr>
+							</thead>
+							<tbody>
+								{
+									listItemCart.map((item, index) => {
+										return <tr key={item.product._id}>
+											<td>
+												<div className="content_product_table">
+													<img className="imagen_product_cart" src={item?.product.imagen} alt="" />
+													<p className="name_product_cart">{item?.product.name}</p>
+												</div>
+											</td>
+											<td>
+												$ {item?.product?.pricePromotion > 0 ? item?.product?.pricePromotion : item?.product?.realPrice}
+											</td>
+											<td>
+												{item?.product?.amount}
+											</td>
+											<td>
+												{item?.amount}
+											</td>
+											<td>
+												$ {item?.product?.pricePromotion > 0 ? item?.product?.pricePromotion * item?.amount : item?.product?.realPrice * item?.amount}
+											</td>
+											<td>
+												<i onClick={() => removeItem(index)} className="uil uil-times icon_delete_product_cart"></i>
+											</td>
+										</tr>
+									})
+								}
+							</tbody>
+						</table>
+						<h3 className="total_price_order">Total:   <span>$ {getTotalPriceCart(listItemCart)}</span></h3>
+						{
+							isActiveCart === false ?
+								<button onClick={(e) => sendOrder(e)} className="btn_cart btn_cart_page">{isLoader === true ? <LoaderButton /> : "Pedir por WhatsApp"}</button>
+								: ""
+						}
+					</>
+					:
+					<section className="content_cart_empty">
+						<h3>Carrito de compras vacio</h3>
+						<button className="btn" onClick={() => { navigate() }}>Ver productos</button>
+					</section>
+			}
+			<ModalOrderInfo alertOrder={alertOrder} isActiveModalInfoOrder={isActiveModalInfoOrder} handlerOpenModalInfoOrder={handlerOpenModalInfoOrder} />
 		</section>
 	);
 };
