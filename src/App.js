3b8c612423935e31bf7d72293f54bbe14599e7ae
Francisco <franciscojosebellosalcedo@gmail.com>
2024-02-13 14:33:39 -0500

    icon whatsapp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Layaut from "./components/layaut/Layaut";
 import ToUp from "./components/topUp/ToUp";
+import WhatsappElement from "./components/whatsappElement/WhatsappElement";
 
 function App() {
 	return (
 		<>
 			<Layaut />
 			<ToUp/>
+			<WhatsappElement/>
 		</>
 	);
 }
---
+++ src/components/whatsappElement/WhatsappElement.jsx
@@ -0,0 +1,11 @@
+import "./WhatsappElement.css";
+
+const WhatsappElement = () => {
+	return (
+		<div className="figure_icon_whatsapp">
+			<i class="uil uil-whatsapp icon_whatsapp"></i>
+		</div>
+	)
+}
+
+export default WhatsappElement;
