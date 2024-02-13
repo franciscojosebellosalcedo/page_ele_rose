a3992f7f70eaadc9903e2b8589ccb1b7f2025540
LauraSD24 <91168646+LauraSD24@users.noreply.github.com>
2024-02-13 18:17:17 -0500

    enlaces what

--- src/components/whatsappElement/WhatsappElement.jsx
+++ src/components/whatsappElement/WhatsappElement.jsx
@@ -3,7 +3,7 @@ import "./WhatsappElement.css";
 const WhatsappElement = () => {
 
 	return (
-		<a href="https://api.whatsapp.com/message/24YJVHRVEYC3P1?autoload=1&app_absent=0" target="_blank" className="figure_icon_whatsapp">
+		<a href="https://w.app/kjb9A6" target="_blank" className="figure_icon_whatsapp">
 			<i class="uil uil-whatsapp icon_whatsapp"></i>
 		</a>
 	)
