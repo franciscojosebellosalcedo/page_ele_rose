cd1865435ed90040312bcf3ff5ae68552668d859
Francisco <franciscojosebellosalcedo@gmail.com>
2024-02-15 17:32:06 -0500

    section page product

--- src/components/infoAditionalProduct/InfoAditionalProduct.jsx
+++ src/components/infoAditionalProduct/InfoAditionalProduct.jsx
@@ -1,32 +1,63 @@
 import { useState } from "react";
 import "./InfoAditionalProduct.css";
+import { INFO_ELEROSE } from "../../constants/constants";
 
 const InfoAditionalProduct = () => {
-	const [openContent,setOpenContent]=useState({
-		description:false,
-		shipping:true,
-		comments:false
+	const [openContent, setOpenContent] = useState({
+		description: true,
+		shipping: false,
+		comments: false
 	})
 
-	const handlerOpenContent=(target)=>{
-		const keys=Object.keys(openContent);
-		const contents=openContent;
+	const handlerOpenContent = (target) => {
+		const keys = Object.keys(openContent);
+		const contents = openContent;
 		for (let index = 0; index < keys.length; index++) {
 			const key = keys[index];
-			contents[key]=false;
+			contents[key] = false;
 		}
-		contents[`${target}`]=true;
-		setOpenContent({...contents});
+		contents[`${target}`] = true;
+		setOpenContent({ ...contents });
 
 	}
 	return (
-		<section className="section_aditional_product">
-			<article className="list_title_info">
-				<h3 onClick={()=>handlerOpenContent("description")} className={`title_item ${openContent.description===true ? "see_element_before_title":""}`}>Descripción</h3>
-				<h3 onClick={()=>handlerOpenContent("shipping")} className={`title_item ${openContent.shipping===true ? "see_element_before_title":""}`}>Envios y pagos</h3>
-				<h3 onClick={()=>handlerOpenContent("comments")} className={`title_item ${openContent.comments===true ? "see_element_before_title":""}`}>Comentarios</h3>
-			</article>
-
+		<section className="">
+			<section className="section_aditional_product">
+				<article className="list_title_info">
+					<h3 onClick={() => handlerOpenContent("description")} className={`title_item ${openContent.description === true ? "see_element_before_title" : ""}`}>Descripción</h3>
+					<h3 onClick={() => handlerOpenContent("shipping")} className={`title_item ${openContent.shipping === true ? "see_element_before_title" : ""}`}>Envios y pagos</h3>
+					<h3 onClick={() => handlerOpenContent("comments")} className={`title_item ${openContent.comments === true ? "see_element_before_title" : ""}`}>Comentarios</h3>
+				</article>
+			</section>
+			{
+				openContent.shipping === true ?
+					<p className="text_info_shipping">{INFO_ELEROSE.infoEleRoseDetails}</p>
+					: ""
+			}
+			{
+				openContent.description === true ?
+					<div className="content_description">
+						<h3 className="title_description">Maximiza la Vida Útil de Tu Producto con los siguientes Consejos</h3>
+						<ul className="list_tips">
+							<li>
+								<p>Evita el contacto con el agua</p>
+							</li>
+							<li>
+								<p>Pontelos después de aplicar perfumes, cremas o productos de belleza</p>
+							</li>
+							<li>
+								<p>Evita hacer ejercicio con ellos</p>
+							</li>
+							<li>
+								<p>Evita el contacto con productos químicos</p>
+							</li>
+							<li>
+								<p>Limpialos con un paño suave y guardalos en un lugar seco después de usarlos</p>
+							</li>
+						</ul>
+					</div>
+					: ""
+			}
 		</section>
 	)
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,43 +1,49 @@
 import NextArrow from "../components/nextArrow/NextArrow.jsx";
 import PrevArrow from "../components/prevArrow/PrevArrow.jsx";
 
-export const ROUTES={
-	INIT:"/",
-	ACCESORIES:"/accesories/all",
-	ONE_CATEGORY:"/accesories/category",
-	PRODUCT:"/accesories/product",
-	CART:"/cart",
-	ACCOUNT:"/account",
-	ABOUT:"/about",
-	CONTACT:"/contact",
-	NOT_FOUND:"/not-found"
-}
+export const ROUTES = {
+	INIT: "/",
+	ACCESORIES: "/accesories/all",
+	ONE_CATEGORY: "/accesories/category",
+	PRODUCT: "/accesories/product",
+	CART: "/cart",
+	ACCOUNT: "/account",
+	ABOUT: "/about",
+	CONTACT: "/contact",
+	NOT_FOUND: "/not-found",
+};
 
-export const INFO_ELEROSE={
-	serviceClient:{
-		title:"Atención al cliente",
-		text:"Atendemos todas las inquietudes por Whatsapp +57 3053559355 y también por el email elerose@gmail.com"
+export const INFO_ELEROSE = {
+	serviceClient: {
+		title: "Atención al cliente",
+		text: "Atendemos todas las inquietudes por Whatsapp +57 3053559355 y también por el email elerose@gmail.com",
 	},
-	deliveries:{
-		title:"Envíos",
-		text:"Envíos seguros a toda Colombia con nuestros aliados logísticos"
+	deliveries: {
+		title: "Envíos",
+		text: "Envíos seguros a toda Colombia con nuestros aliados logísticos",
 	},
-	ratingsClients:{
-		title:"Descubre lo que dicen nuestros clientes"
+	ratingsClients: {
+		title: "Descubre lo que dicen nuestros clientes",
 	},
-	messageProductsNotFound:"No hay productos"
-}
+	messageProductsNotFound: "No hay productos",
+	infoEleRoseDetails: `
+		Hacemos domicilios en Arjona y envíos a toda Colombia mediante transportadoras como Envía,
+		Interrapidísimo o Servientrega. Costo del domicilio en Arjona: $4.000, fuera de Arjona: desde $8.000,
+		se indicará el valor exacto al hacer la solicitud de pedido dependiendo de la ciudad de destino.
+		El pago del producto más envío puede ser realizado por Nequi o Daviplata.
+	`,
+};
 
-export const LIST_NUMBER_SCORE=[1,2,3,4,5];
+export const LIST_NUMBER_SCORE = [1, 2, 3, 4, 5];
 
-export const URL_BASE=process.env.REACT_APP_URL_API;
+export const URL_BASE = process.env.REACT_APP_URL_API;
 
-export const SETTINGS_SLIDER_RESPONSIVE_RATINGS={
+export const SETTINGS_SLIDER_RESPONSIVE_RATINGS = {
 	dots: true,
 	infinite: false,
 	speed: 500,
 	slidesToShow: 4,
-	autoplay:true,
+	autoplay: true,
 	slidesToScroll: 4,
 	initialSlide: 0,
 	responsive: [
@@ -46,144 +52,144 @@ export const SETTINGS_SLIDER_RESPONSIVE_RATINGS={
 			settings: {
 				slidesToShow: 3,
 				slidesToScroll: 3,
-			}
+			},
 		},
 		{
 			breakpoint: 740,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 710,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 690,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 670,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 650,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 630,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 610,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 590,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 580,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 560,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 540,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 530,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 510,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 500,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 460,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1
-			}
+				slidesToScroll: 1,
+			},
 		},
 		{
 			breakpoint: 450,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1
-			}
+				slidesToScroll: 1,
+			},
 		},
 		{
 			breakpoint: 430,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1
-			}
+				slidesToScroll: 1,
+			},
 		},
 		{
 			breakpoint: 410,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1
-			}
+				slidesToScroll: 1,
+			},
 		},
 		{
 			breakpoint: 400,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		}
-	]
+				slidesToScroll: 1,
+			},
+		},
+	],
 };
-export const SETTINGS_SLIDER_RESPONSIVE={
+export const SETTINGS_SLIDER_RESPONSIVE = {
 	dots: true,
 	infinite: false,
 	speed: 500,
@@ -196,171 +202,170 @@ export const SETTINGS_SLIDER_RESPONSIVE={
 			settings: {
 				slidesToShow: 3,
 				slidesToScroll: 3,
-			}
+			},
 		},
 		{
 			breakpoint: 740,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 710,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 690,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 670,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 650,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 630,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 610,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 590,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 580,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 560,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 540,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 530,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 510,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 500,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
-			}
+			},
 		},
 		{
 			breakpoint: 460,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1
-			}
+				slidesToScroll: 1,
+			},
 		},
 		{
 			breakpoint: 450,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1
-			}
+				slidesToScroll: 1,
+			},
 		},
 		{
 			breakpoint: 430,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1
-			}
+				slidesToScroll: 1,
+			},
 		},
 		{
 			breakpoint: 410,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1
-			}
+				slidesToScroll: 1,
+			},
 		},
 		{
 			breakpoint: 400,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		}
-	]
+				slidesToScroll: 1,
+			},
+		},
+	],
 };
-export const SETTINGS_SLIDER_RESPONSIVE_CATEGORIES_ACCESORIES={
+export const SETTINGS_SLIDER_RESPONSIVE_CATEGORIES_ACCESORIES = {
 	dots: true,
 	speed: 500,
 	slidesToShow: 4,
-	autoplay:true,
+	autoplay: true,
 	slidesToScroll: 4,
 	initialSlide: 0,
-	infinite:true,
+	infinite: true,
 	responsive: [
 		{
 			breakpoint: 700,
 			settings: {
 				slidesToShow: 5,
-				slidesToScroll: 5
-			}
-		}
-		,
+				slidesToScroll: 5,
+			},
+		},
 		{
 			breakpoint: 400,
 			settings: {
 				slidesToShow: 3,
-				slidesToScroll: 3
-			}
-		}
-	]
+				slidesToScroll: 3,
+			},
+		},
+	],
 };
 
-export const SETTINGS_SLIDER_MAIN={
+export const SETTINGS_SLIDER_MAIN = {
 	dots: true,
 	infinite: true,
 	speed: 500,
@@ -371,9 +376,9 @@ export const SETTINGS_SLIDER_MAIN={
 	prevArrow: <PrevArrow />,
 };
 
-export const HEADERS={
+export const HEADERS = {
 	"Access-Control-Allow-Origin": "*",
 	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
 	"Access-Control-Allow-Headers": "Content-Type",
-	"Content-Type":"application/json"
-}
+	"Content-Type": "application/json",
+};
