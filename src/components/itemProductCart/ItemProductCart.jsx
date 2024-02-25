2f39a63d78170448acb959590e98049c76d36a62
LauraSD24 <91168646+LauraSD24@users.noreply.github.com>
2024-02-24 21:40:58 -0500

    flux amount cart

--- src/components/itemProductCart/ItemProductCart.jsx
+++ src/components/itemProductCart/ItemProductCart.jsx
@@ -1,11 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import { decrementAmountItem, incrementAmountItem, removeItemCart, setActiveCart, setAmountItemCart } from "../../features/cart/cart";
 import "./ItemProductCart.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ItemProductCart = ({item,index}) => {
 	const cart=useSelector((state)=>state.cart.data.list);
-	const [num,setNum]=useState(1);
+	let [amount,setAmount]=useState(1);
+	const [isValidAmount,setIsValidAmount]=useState(true);
 	const dispatch=useDispatch();
 
 	const removeItem=()=>{
@@ -15,32 +16,49 @@ const ItemProductCart = ({item,index}) => {
 		}
 	}
 
-	const decrementAmount=(index,amount)=>{
-		dispatch(decrementAmountItem({index,amount}));
+	const handlerAmount=(value)=>{
+		if(value===""){
+			dispatch(setAmountItemCart({product:item.product,amount:0}));
+			setAmount("");
+			setIsValidAmount(false);
+		}else{
+			const num=parseInt(value);
+			if(num<0 || num>item.product.amount){
+				setIsValidAmount(false);
+				dispatch(setAmountItemCart({product:item.product,amount:0}));
+			}else{
+				setIsValidAmount(true);
+				dispatch(setAmountItemCart({product:item.product,amount:num}));
+			}
+			setAmount(num);
+		}
 	}
 
-	const incremetAmount=(index,amount)=>{
-		dispatch(incrementAmountItem({index,amount}));
+	const decrementAmount=(index,num)=>{
+		dispatch(decrementAmountItem({index,amount:num}));
+		if(cart[index].amount-num>0){
+			const newAmount=parseInt(amount)-num;
+			setAmount(newAmount);
+		}
 	}
 
-	// const setAmountItem=(value,item)=>{
-	// 	const valueAmount=parseInt(value);
-	// 	if(value === "" ){
-	// 		dispatch(setAmountItemCart({amount:1,product:item.product}));
-	// 	}else if(valueAmount < 1){
-	// 		dispatch(setAmountItemCart({amount:1,product:item.product}));
-	// 	}
-	// 	else if(valueAmount>item.product.amount){
-	// 		dispatch(setAmountItemCart({amount:item.product.amount,product:item.product}));
-	// 	}
-	// 	else{
-	// 		dispatch(setAmountItemCart({amount:valueAmount,product:item.product}));
-	// 	}
-	// }
+	const incremetAmount=(index,num)=>{
+		dispatch(incrementAmountItem({index,amount:num}));
+		if((cart[index].amount+num)<=item.product.amount){
+			const newAmount=parseInt(amount)+num;
+			setAmount(amount=== ""? 0 :);
+		}
+	}
 
+	useEffect(()=>{
+		if(cart.length>0){
+			setAmount(cart[index].amount);
+		}
+	},[]);
 
 	return (
-		<div className="content_product_cart">
+		<section className="container_item_cart">
+			<div className="content_product_cart">
 			<img className="img_product_cart" src={item?.product?.imagen} alt="" />
 			<div className="section_data_product_cart">
 				<p className="text_item_cart name_item_cart">{item?.product?.name} </p>
@@ -50,16 +68,23 @@ const ItemProductCart = ({item,index}) => {
 						<p className="text_nowrap text_item_cart amout_item_cart">Qty: {item?.amount}</p>
 						<p className="text_nowrap text_item_cart price_item_cart">$ {item?.product?.pricePromotion >0 ? item?.product?.pricePromotion :item?.product?.realPrice}</p>
 					</div>
-					<input value={1} onInput={(e)=>{console.log(e.target.value)}}  className="input_amount"  type="number" name="" id="" />
+					<input onInput={(e)=>handlerAmount(e.target.value)} value={amount}  className="input_amount"  type="number"/>
 					<div className="controllers">
-						<i onClick={()=>setNum(7)} className="uil uil-arrow-up icon_controller"></i>
-						<i className="uil uil-arrow-down icon_controller"></i>
+						<i onClick={()=>incremetAmount(index,1)}  className="uil uil-arrow-up icon_controller"></i>
+						<i onClick={()=>decrementAmount(index,1)}  className="uil uil-arrow-down icon_controller"></i>
 					</div>
 				</section>
 			</div>
+			
 
 			<i className="uil uil-times icon_delete_item_cart" onClick={()=>removeItem()}></i>
 		</div>
+		{
+			isValidAmount === false ?
+				<p className="error_amount error_item_cart">Cantidad ingresada incorrecta</p>
+			:""
+		}
+		</section>
 	)
 }
 
